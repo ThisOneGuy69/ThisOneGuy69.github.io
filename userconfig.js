5abbfacadb23a2cb877e730c5c4629e2d26c6b75
Nothanks <123861623+ThisOneGuy69@users.noreply.github.com>
2025-04-16 22:36:59 +0000

    Update userconfig.js

--- userconfig.js
+++ userconfig.js
@@ -29,7 +29,7 @@ const default_config = {
               icon_color: palette.green,
             },
             {
-              name: "musicForProgramming();",
+              name: "musicForProgramming",
               url: "https://musicforprogramming.net",
               icon: "binary-tree",
               icon_color: palette.peach,
